ddb38183c9fe31c0cbe8e2a51f00e775c8da2720
augustineezinwa <augustineezinwa@gmail.com>
2018-05-30 10:46:09 +0100

    ft(login-endpoint-API): Create login endpoint -Authenticate user during login -Create login route -validate login credentials [finishes #157948727]

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import connect from '../connections/connect';
-import { createUserData } from '../helper/instructions/userInstructions';
+import { createUserData, findEmail } from '../helper/instructions/userInstructions';
 import ErrorHandler from '../helper/ErrorHandler';
 
-const { handleDatabaseConnectionError, handleTableWriteError } = ErrorHandler;
+const { handleTableWriteError } = ErrorHandler;
 
 dotenv.config();
 /**
@@ -49,6 +50,51 @@ class UserController {
         });
       })
       .catch(err => handleTableWriteError(err, res));
+  }
+  /**
+  * @description -This method logins users and admin into Maintenance-Tracker
+  *
+  * @param {object} req - The request payload sent to the router
+  * @param {object} res - The response payload sent back from the controller
+  *
+  * @returns {object} - status Message and logins user into Maintenance-Tracker
+  *
+  * @description This controller authenticates a user during login using jwt token.
+  * @memberOf UserController
+  * @static
+  */
+  static loginUser(req, res) {
+    connect.query(findEmail(req.body.email))
+      .then((data) => {
+        const {
+          id, email, admin, password
+        } = data.rows[0];
+        const payload = {
+          id,
+          email,
+          adminStatus: admin
+        };
+        if (bcrypt.compareSync(req.body.password, password)) {
+          const token = jwt.sign({ payload }, process.env.PRIVATE_KEY, { expiresIn: 77 * 77 });
+          return res.status(200).json({
+            status: 'success',
+            data: {
+              message: 'you are logged in', token
+            }
+          });
+        }
+        return res.status(401).json({
+          status: 'fail',
+          data: {
+            message: 'password is incorrect!'
+          }
+        });
+      }).catch(err => res.status(404).json({
+        status: 'fail',
+        data: {
+          message: 'you cant login at this time, email doesnt exist'
+        }
+      }));
   }
 }
 
--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -3,9 +3,10 @@ import UserController from '../controllers/UserController';
 import UserValidation from '../middlewares/UserValidation';
 
 const { validatesignUp, checkEmail } = UserValidation;
-const { signupUser } = UserController;
+const { signupUser, loginUser } = UserController;
 const userRouter = express.Router();
 
 userRouter.post('/auth/signup', validatesignUp, checkEmail, signupUser);
+userRouter.post('/auth/login', loginUser);
 
 export default userRouter;
